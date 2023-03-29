3cac369909bdbce284af3096794a8cecdac07166
WG67 <Withergaming67@gmail.com>
2023-03-29 09:53:06 +0100

    Update server.ts

--- my-rss-app/server.ts
+++ my-rss-app/server.ts
@@ -1,5 +1,7 @@
 import { serve } from "https://deno.land/std@0.181.0/http/server.ts";
+import {serveFile} from "https://deno.land/std@0.181.0/http/file_server.ts"
 
+const server = serve({ port:8000 })
 const port = 8080;
 
 for await (const req of server) {
@@ -9,3 +11,6 @@ for await (const req of server) {
     req.respond(response)
   }
 }
+
+console.log(`HTTP webserver running. Access it at: http://localhost:8080/`);
+await serve(handler, { port });
